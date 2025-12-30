001f336cf2270d6de4fc44b274e2b0f0583ad93c
Thuria234naing <113816550+theYE-8266-viren-Nai@users.noreply.github.com.>
2025-12-30 09:40:54 +0630

    First commit into the repo

---
+++ FirstProject/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GameHeader from './components/GameHeader'
+
+const App = () => {
+  return (
+    <>
+        <GameHeader/>
+    </>
+  )
+}
+
+export default App
---
+++ FirstProject/src/components/GameHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GameHeader = () => {
+  return (
+    <>
+    <div className="game-header">
+        <h1>Memory Card Game</h1>
+    </div>
+    </>
+  )
+}
+
+export default GameHeader
